4115e11972daee90efd9a051bd1103294b9ecd77
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-26 20:03:29 +0100

    initial commit

--- Views/NavBar.swift
+++ Views/NavBar.swift
@@ -10,57 +10,31 @@ import SwiftUI
 struct NavBar: View {
     
     
-     @Binding var showMenu: Bool
-     var title: String = "Apple Card"
-     var showSearchField = true
+    @Binding var showMenu: Bool
+    var title: String = "Apple Card"
+    var showSearchField = true
     
     @State private var searchTerm = ""
     
-        
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             HStack {
-                
-                Rectangle()
-                    .foregroundColor(Color.clear)
-                    .frame(width: 20, height: 20)
-                    .padding(.leading)
-                Spacer()
+
                 Text(title)
                     .font(.title)
                     .bold()
                     .foregroundColor(Color.white)
-                
-                Spacer()
-                
-                Button(action: {
-                    HapticFeedback.generate()
-                    self.showMenu.toggle()
-                }) {
-
-                Image(systemName: "line.horizontal.3")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                    .imageScale(.large)
-                    .foregroundColor(Color.white)
-                    .padding(.trailing)
-                }
-                    
-            }.padding(.bottom, 20)
+            }
+            
+        }.padding(.bottom, 10)
             .frame(maxWidth: .infinity)
             .frame(height: showSearchField ? 140 : 90,alignment: showSearchField ? .center : .bottom)
             .background(LinearGradient(colors: [.purple, .pink], startPoint: .topLeading, endPoint: .bottomTrailing))
-             .clip(shouldCurve: showSearchField)
-            
-//            if showSearchField{
-//                LCSearchField(value: self.$searchTerm)
-//                               .padding()
-//                               .offset(y: 15)
-//                       }
-        }
+            .clip(shouldCurve: showSearchField)
+        
     }
 }
-
 struct NavBar_Previews: PreviewProvider {
     static var previews: some View {
         NavBar(showMenu: .constant(false)).previewLayout(.sizeThatFits)
